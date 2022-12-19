51fcfa17155c22a3f8110988804c8bcecbb7cfb7
MiniMini-On <120891914+MiniMini-On@users.noreply.github.com>
2022-12-19 11:38:33 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ $(function () {
     $(".overlay").fadeOut();
   });
 
+  $(".navbar_toggleBtn").click(function (e) {
+    e.preventDefault();
+  });
+
   //************student 섹션의 버튼 동작*******************************//
   //오른쪽 계속 클릭하다가 끝점에 닿으면 처음으로 돌아감, 부드럽게 이동하는 애니메이션 적용
   //전체 이미지(10장)가 차지하는 너비 = 1982
@@ -42,13 +46,14 @@ $(function () {
   //연락처에 입력값이 8자리 이하일 때, 알파벳&한글&특수문자 포함할 때는 경고창 발생, 연락처 입력값 삭제
   //개인정보이용동의 미체크시 경고창 발생
   //위에 해당 사항 없을 시 신청완료 알림창 발생
+
   $("#applybtn").on("click", function () {
     let name = $("#name").val();
     let phone = $("#phone").val();
     let pattern1 = /[0-9]/;
     let pattern2 = /[a-zA-Z]/;
     let pattern3 = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
-    let pattern4 = /[~!@#\#$%<>^&*]/;
+    let pattern4 = /[~!@#\#$%<>^&*,]/;
     let pattern5 = /[가-힣]/;
 
     name = name.split(" ").join("");
@@ -60,6 +65,8 @@ $(function () {
       .split(".")
       .join("")
       .split("(")
+      .join("")
+      .split(" ")
       .join("");
 
     if (
